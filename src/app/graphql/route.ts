f7ca1267eb1c6d84b60db5ad3b8608e5ea6bf14e
Shashkov Danil <shashkovdanil@gmail.com>
2023-08-09 15:05:58 +0100

    fix schema path

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -7,7 +7,11 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const typeDefs = readFileSync('./schema.graphql', 'utf8')
+const schemaPath =
+  process.env.NODE_ENV === 'development'
+    ? './public/schema.graphql'
+    : './schema.graphql'
+const typeDefs = readFileSync(schemaPath, 'utf8')
 
 const server = new ApolloServer<Resolvers>({
   resolvers: {
